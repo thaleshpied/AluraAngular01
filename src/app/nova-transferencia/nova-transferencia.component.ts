d38a11d908da69532e49aca84c80f3ba90fa5121
Thales <thaleshpied@gmail.com>
2021-09-22 18:39:13 -0300

    03 - Exportando dados do componente
    Exportando dados

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,13 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  @Output() aoTranferir = new EventEmitter<any>();
 
   valor: number = 0;
   destino: number = 1;
 
   transferir() {
     console.log('Solicitada nova transferência');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTranferir.emit(valorEmitir);
   }
 }
