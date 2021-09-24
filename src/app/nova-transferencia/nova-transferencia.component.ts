eafae6267221733f8b4c0a719810bc7cccf5d6f2
Thales <thaleshpied@gmail.com>
2021-09-23 21:14:33 -0300

    Correção: Importando EventEmitter para o Angular

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output } from '@angular/core';
-import { EventEmitter } from 'stream';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
