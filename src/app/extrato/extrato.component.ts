f1cb56660c01e8a68e84e5fc3292140deb9d04eb
Thales <thaleshpied@gmail.com>
2021-10-01 11:22:57 -0300

    05.01 - Criação de Service

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TransferenciaService } from '../service/transferencia.service';
 
 @Component({
   selector: 'app-extrato',
@@ -9,9 +10,10 @@ export class ExtratoComponent implements OnInit {
 
   @Input() transferencias: any[];
 
-  constructor() { }
+  constructor(private service: TransferenciaService) { }
 
   ngOnInit(): void {
+    this.transferencias = this.service.transferencias;
   }
 
 }
